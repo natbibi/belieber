a732fd7e777b7d28be7f4e210568ba489b9b64a5
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-12 18:05:43 +0100

    add likeButton for each album which changes color on click

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LikeButton() {
+    const [liked, setLiked] = useState('false')
+
+
+    function handleLikeButton(e) {
+        e.stopPropagation()
+        setLiked(!liked)
+
+        // if (LikeButton.textContent === '💜') {
+        //     (LikeButton.textContent === '💖')
+        // } else {
+        //     (LikeButton.textContent === '💜')
+        // }
+
+    }
+
     return (
-        <>
-            <button>💜</button>
-        </>
+        <div>
+            <span role="switch" onClick={handleLikeButton} style={{ fontSize: '5rem', color: liked ? 'lightgrey' : 'hotpink' }}>❤︎</span>
+        </div>
     )
 }
 
