3b5ac3faa06411fa63d3b9b41d6aefe64a373e45
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-14 10:22:46 +0100

    change button unclicked to pink

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -4,16 +4,16 @@ function LikeButton(props) {
     let [liked, setLiked] = useState('false')
     let [likeCounter, setLikeCounter] = useState(0)
 
-            
+
 
     function handleLikeButton(e) {
         e.stopPropagation()
         setLiked(!liked)
         setLikeCounter(likeCounter + 1)
 
-       let albumName = props.name
-            console.log(albumName)
-            console.log(++likeCounter)
+        let albumName = props.name
+        console.log(albumName)
+        console.log(++likeCounter)
 
     }
 
@@ -21,10 +21,10 @@ function LikeButton(props) {
     return (
 
         <div className="heart-like">
-            <span role="switch" onClick={handleLikeButton} style={{ fontSize: '5rem', color: liked ? 'lightgrey' : 'hotpink' }}><i className="far fa-heart"></i></span>
-    <p>{likeCounter} likes </p>
-            
-            
+            <span role="switch" onClick={handleLikeButton} style={{ fontSize: '5rem', color: liked ? 'pink' : 'hotpink' }}><i className="far fa-heart"></i></span>
+            <p>{likeCounter} likes </p>
+
+
         </div>
     )
 }
