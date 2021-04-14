dba5475ef7ce388a9d86a595d9d9a8071bfcda22
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-14 22:44:49 +0100

    remove extra comma in seeds

--- src/pages/JBMusic/index.js
+++ src/pages/JBMusic/index.js
@@ -5,7 +5,7 @@ import { Albums, Greeting, Counter } from '../../components'
 const JBMusic = () => {
     const [album, setAlbum] = useState([
         { id: 1, albumCode: '3BmcYMh0KYsimWL6p2gPa9', name: 'My World 2.0', img: "https://upload.wikimedia.org/wikipedia/en/b/b9/Myworld2.jpg", releaseDate: 2010, title: 'Baby' },
-        { id: 2., albumCode: '63MKT9hwmiMFFdFp5SdB1p', name: 'Under The Mistletoe', img: "https://upload.wikimedia.org/wikipedia/en/4/49/Under_the_Mistletoe.jpg", releaseDate: 2011, title: 'Mistletoe' },
+        { id: 2, albumCode: '63MKT9hwmiMFFdFp5SdB1p', name: 'Under The Mistletoe', img: "https://upload.wikimedia.org/wikipedia/en/4/49/Under_the_Mistletoe.jpg", releaseDate: 2011, title: 'Mistletoe' },
         { id: 3, albumCode: '70f70xLCpH7wHaVvB2oZT9', name: 'Believe', img: "https://upload.wikimedia.org/wikipedia/en/0/01/Believe-JB-Album.jpg", releaseDate: 2012, title: 'Boyfriend' },
         { id: 4, albumCode: '6Fr2rQkZ383FcMqFyT7yPr', name: 'Purpose', img: "https://upload.wikimedia.org/wikipedia/en/2/27/Justin_Bieber_-_Purpose_%28Official_Album_Cover%29.png", releaseDate: 2015, title: 'Love-yourself' },
         { id: 5, albumCode: '63iWSELt9V1kV6RSMxN7Ii', name: 'Changes', img: "https://upload.wikimedia.org/wikipedia/en/1/16/Justin_Bieber_-_Changes.png", releaseDate: 2020, title: 'Intentions' },
