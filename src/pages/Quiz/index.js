f5e551f1e7e53df554f63b93ddabb1f7669898f4
sem-the-dev <semhar.tesfu@outlook.com>
2021-04-16 16:01:02 +0100

    add interval

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,55 +1,86 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
+import "./style.css";
 
-function Quiz(){
-    const [showAnswer, setShowAnswer ] = useState();
-    const [questions, setQuestions ] = useState()
+function Quiz() {
+  // const [showAnswer, setShowAnswer ] = useState();
+  // const [questions, setQuestions ] = useState()
+  const [index, setIndex] = useState(0);
 
+  const facts = [
+    {
+      id: 1,
+      question: "What is Justin's half-brother's name?",
+      answer: "Jaxon",
+    },
+    {
+      id: 2,
+      question: "Who did Justin date before Selena?",
+      answer: "Jasmine Villegas",
+    },
+    {
+      id: 3,
+      question:
+        "Which singer also wanted to mentor Justin, although Usher ultimately won out?",
+      answer: "Justin Timberlake",
+    },
+    {
+      id: 4,
+      question: "Which Canadian city is Justin from?",
+      answer: "Stratford",
+    },
+    {
+      id: 5,
+      question:
+        "Justin has a diamond necklace with a pendant of which cartoon character?",
+      answer: "Stewie from Family Guy",
+    },
+    {
+      id: 6,
+      question: "Which TV show did Justin guest star in?",
+      answer: "C.S.I",
+    },
+    {
+      id: 7,
+      question: "Which TV show did Justin guest star in?",
+      answer: "C.S.I",
+    },
+    {
+      id: 8,
+      question: "Which TV show did Justin guest star in?",
+      answer: "C.S.I",
+    },
+    {
+      id: 9,
+      question: "Which TV show did Justin guest star in?",
+      answer: "C.S.I",
+    },
 
-    let i = (Math.floor(Math.random() * 10))
+    {
+      id: 10,
+      question: "Which TV show did Justin guest star in?",
+      answer: "C.S.I",
+    },
+  ];
 
+  useEffect(() => {
+    function getFact() {
+      setIndex(Math.floor(Math.random() * 10));
+    }
 
-    useEffect(() => {
-         function getFact() {
-            try {
-                 let facts = [
-                    {id: 1, question: "What is Justin's half-brother's name?", answer: "Jaxon"},
-                    {id: 2, question: "Who did Justin date before Selena?", answer: "Jasmine Villegas"},
-                    {id: 3, question: "Which singer also wanted to mentor Justin, although Usher ultimately won out?", answer: "Justin Timberlake"},
-                    {id: 4, question: "Which Canadian city is Justin from?", answer: "Jasmine Stratford"},
-                    {id: 5, question: "Justin has a diamond necklace with a pendant of which cartoon character?", answer: "Stewie from Family Guy"},
-                    {id: 6, question: "Which TV show did Justin guest star in?", answer: "C.S.I"},
-                    {id: 7, question: "Which TV show did Justin guest star in?", answer: "C.S.I"},
-                    {id: 8, question: "Which TV show did Justin guest star in?", answer: "C.S.I"},
-                    {id: 9, question: "Which TV show did Justin guest star in?", answer: "C.S.I"},
-                ]
-        
-                setQuestions(facts[i].question)
-                setShowAnswer(facts[i].answer)
-            } catch (err) {
-                ssetQuestions('oops')
-            }
+    getFact();
 
-            
-        }
-        getFact()
+    const stream = setInterval(getFact, 10000);
+    return () => clearInterval(stream);
+  }, []);
 
-        const stream = setInterval(getFact, 5000)
-        console.log(stream)
-        return () => clearInterval(stream)
-    }, [])
-    
-  
+  return (
+    <div>
+      <h1>How well do you know JB?</h1>
 
-    return(
-        <>
-        
-        <h1>How well do you know JB?</h1>
-        
-        <p>{questions}</p>
-        <p>{showAnswer}</p>
-
-        </>
-    )
+      <h3 id="question">{facts[index].question}</h3>
+      <p id="reveal-answer"> {facts[index].answer}</p>
+    </div>
+  );
 }
 
-export default Quiz
+export default Quiz;
